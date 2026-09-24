9dba047fbe8d906d88950486ef39a9411d53021a
vickykumarvikas18vk <vickykumarvikas18vk@gmail.com>
2026-09-24 11:36:58 +0530

    Update course-details.js

--- js/course-details.js
+++ js/course-details.js
@@ -1639,8 +1639,8 @@ async function downloadCoursePDF() {
 
         const marginLeft = 10;
         const marginRight = 10;
-        const marginTop = 15;
-        const marginBottom = 15;
+        const marginTop = 10;
+        const marginBottom = 11;
 
 
         const printableWidth =
